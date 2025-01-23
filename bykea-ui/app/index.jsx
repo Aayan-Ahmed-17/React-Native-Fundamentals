16d63e3c1fbaf2246a59f1b390faa057a33c01ad
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-23 15:57:40 +0500

    set height to header and heroSec comp

--- bykea-ui/app/index.jsx
+++ bykea-ui/app/index.jsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    //Whole App
+    <View style={styles.container}>
+      <View style={styles.header}>
+
+      </View>
+      <View style={styles.heroSection}></View>
+      {/* <StatusBar barStyle="dark-content" backgroundColor="#F0F0F0" /> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    gap: 10
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  header: {
+    height: "10%",
+    backgroundColor: "#000",
+    width: "100%"
+  },
+  heroSection: {
+    flex: 1,
+    backgroundColor: "#00f"
+  }
+});
